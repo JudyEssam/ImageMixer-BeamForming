77955c54ca1546234253b7f9c1c75125b1b70d45
Laila Khaled Mohamed <128589631+LailaKhaled352@users.noreply.github.com>
2024-12-17 21:24:29 +0200

    changes in beam mainWindow and UI

--- BeamForming/MainWindow.py
+++ BeamForming/MainWindow.py
@@ -14,10 +14,23 @@ class MainWindow1(QMainWindow):
         os.chdir(os.path.dirname(os.path.abspath(__file__)))
         loadUi("../MainWindow.ui", self)
 
-        self.label = self.findChild(QLabel, "label")
-        self.label.setVisible(False)
-        self.spinbox = self.findChild(QSpinBox, "spinBox")
-        self.spinbox.setVisible(False)
+        self.phase_label = self.findChild(QLabel, "Phase_2")
+
+        #for x and y location
+        self.loc_x = self.findChild(QLabel, "locX")
+        self.loc_x.setVisible(False)
+        self.spinbox_x = self.findChild(QSpinBox, "spinBox_X")
+        self.spinbox_x.setVisible(False)
+
+        self.loc_y = self.findChild(QLabel, "locY")
+        self.loc_y.setVisible(False)
+        self.spinbox_y = self.findChild(QSpinBox, "spinBox_Y")
+        self.spinbox_y.setVisible(False)
+
+        # Multi arrays check
+        self.multi_array = self.findChild(QRadioButton, 'radioButton1_3')
+        self.multi_array.toggled.connect(self.toggleBeamAngleMode)
+
         # Multi arrays check
         self.multi_array = self.findChild(QRadioButton, 'radioButton1_3')
         self.multi_array.toggled.connect(self.toggleBeamAngleMode)
@@ -34,7 +47,11 @@ class MainWindow1(QMainWindow):
         self.spacing_spinbox=self.findChild(QDoubleSpinBox, 'elements_spacing')
         self.elements_num_spinbox=self.findChild(QSpinBox, 'elements_no')
         self.beam_angle_spinbox=self.findChild(QSpinBox, 'beam_angle')
+        self.beam_angle_spinbox.setVisible(False)
+        self.beamLabel = self.findChild(QLabel, "beam_label")
+        self.beamLabel.setVisible(False)
         self.shape_combox= self.findChild(QComboBox, 'Shape_comboBox')
+        self.shape_combox.currentIndexChanged.connect(self.updateLabelForShape)
 
        #for element parameters
         self.isotropic_checkbox= self.findChild(QCheckBox, 'Isotropic_checkbox')
@@ -56,27 +73,37 @@ class MainWindow1(QMainWindow):
         self.mode=None
         self.array=None
         self.signal= Signal()
+        
+
+    def updateLabelForShape(self, index):
+        if index == 1:  # "Circular" is the second item (index 1)
+            self.phase_label.setText("Radius: ")
+        else:  # Default to "Linear" (index 0)
+            self.phase_label.setText("Element Spacing: ")     
 
     def toggleBeamAngleMode(self, checked):
         """Toggle between Beam Angle and Location input."""
         if checked:
             # Change label text
-            self.beam_label.setText("Location_x: ")
-            self.label.setText("Location_y")
-            self.label.setVisible(True)
-            self.spinbox.setVisible(True)
+            self.loc_x.setVisible(True)
+            self.loc_y.setVisible(True)
+            self.spinbox_x.setVisible(True)
+            self.spinbox_y.setVisible(True)
 
         else:
             # Revert to Beam Angle mode
-            self.beam_label.setText("Beam Angle: ")
-            self.label.setVisible(False)
-            self.spinbox.setVisible(False)
+            self.loc_x.setVisible(False)
+            self.loc_y.setVisible(False)
+            self.spinbox_x.setVisible(False)
+            self.spinbox_y.setVisible(False)
 
     def selectMode(self, index):
         if index==0:
             self.mode= TransmissionMode(self)
         elif index==1:
-            self.mode= RecievingMode()
+            self.beamLabel.setVisible(True)
+            self.beam_angle_spinbox.setVisible(True)
+            # self.mode= RecievingMode()
 
     def formArray(self):
         antennas_num= self.elements_num_spinbox.value()
